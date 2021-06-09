984b15ace259f6476357c66e53a5e02f1165537c
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-06-09 02:45:01 +0000

    Version 1.0: Desarrollo del header

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,21 +4,57 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
+function Header() {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container d-flex">
+				<a className="navbar-brand" href="#">
+					Start Bootstrap
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarTogglerDemo02"
+					aria-controls="navbarTogglerDemo02"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<ul className="nav justify-content-end ml-auto">
+					<li className="nav-item">
+						<a className="nav-link text-white" href="#">
+							Home
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-secondary" href="#">
+							About
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-secondary" href="#">
+							Services
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-secondary" href="#">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
+
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid m-0 p-0">
+			<Header />
+			<div className="container"></div>
 		</div>
 	);
 }
